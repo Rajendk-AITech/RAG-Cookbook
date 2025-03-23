4043ba322484f3ba7e67b00782ab1e440c57edad
Rajendk-AITech <smartaisolutions.tech@gmail.com>
2025-03-23 15:51:30 -0400

    Added links
    Added links

--- Docling_Main/docling/Step1-extraction.py
+++ Docling_Main/docling/Step1-extraction.py
@@ -5,21 +5,25 @@ converter = DocumentConverter()
 
 # --------------------------------------------------------------
 # Basic PDF extraction
+# Document 1
+# Original Document - https://www.safetyforward.com/docs/legal.pdf
+# Document 2
+# https://www.apple.com/newsroom/pdfs/fy2024-q1/FY24_Q1_Consolidated_Financial_Statements.pdf
+# This document is about not using mobile phones while driving a motor vehicle and prohibits disabling its motion restriction features.
 # --------------------------------------------------------------
 
-result = converter.convert("https://arxiv.org/pdf/2408.09869")
+result = converter.convert("https://www.apple.com/newsroom/pdfs/fy2024-q1/FY24_Q1_Consolidated_Financial_Statements.pdf")
 
 document = result.document
 markdown_output = document.export_to_markdown()
 json_output = document.export_to_dict()
-
 print(markdown_output)
 
 # --------------------------------------------------------------
 # Basic HTML extraction
 # --------------------------------------------------------------
 
-result = converter.convert("https://ds4sd.github.io/docling/")
+result = converter.convert("https://python.langchain.com/docs/introduction/")
 
 document = result.document
 markdown_output = document.export_to_markdown()
@@ -29,7 +33,7 @@ print(markdown_output)
 # Scrape multiple pages using the sitemap
 # --------------------------------------------------------------
 
-sitemap_urls = get_sitemap_urls("https://ds4sd.github.io/docling/")
+sitemap_urls = get_sitemap_urls("https://www.langchain.com/")
 conv_results_iter = converter.convert_all(sitemap_urls)
 
 docs = []
